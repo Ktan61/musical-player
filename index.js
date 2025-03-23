1f65fea8ea67597d660104ef76f0ed4bad8cc2ff
MarloweReyes <mreyes58@my.bcit.ca>
2025-03-23 00:52:49 -0700

    adding README.md file

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const db = new pg.Client({
     user: "postgres",  
     host: "localhost", 
     database: "musical-player", 
-    password: "1234",  // Database password
+    password: "marno",  // Database password
     port: 5432,        
 });
 db.connect();  // Establish the connection to the database
